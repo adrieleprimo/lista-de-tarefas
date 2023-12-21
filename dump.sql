d69645bf4763c29f18c9a13b5acdfc5046e72d48
Adriele Primo <adriele.dejprimo@gmail.com>
2023-12-21 16:26:52 -0300

    Criação do banco de dados

---
+++ dump.sql
@@ -0,0 +1,13 @@
+CREATE TABLE tarefas(
+  id SERIAL PRIMARY KEY,
+  titulo TEXT NOT NULL,
+  descricao TEXT,
+  data_conclusao DATE
+  );
+
+CREATE TABLE usuarios(
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(100) NOT NULL
+  );

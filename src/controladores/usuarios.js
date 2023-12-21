2998aa8efca792f6e26c77326f271c03bd6567fb
Adriele Primo <adriele.dejprimo@gmail.com>
2023-12-21 17:04:45 -0300

    Criação de login de usuarios

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -1,5 +1,8 @@
 const knex = require('../conexao');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const senhaJwt = process.env.JWT_HASH;
+
 const cadastrarUsuario = async(req, res)=>{
     const {nome, email, senha} = req.body;
 
@@ -7,7 +10,7 @@ const cadastrarUsuario = async(req, res)=>{
         const usuarioEmail = await knex('usuarios').where({email}).first();
 
         if(usuarioEmail){
-            return res.status(400).json({mensagem: 'E-mail já cadastrado'});
+            return res.status(400).json({mensagem: 'E-mail já cadastrado.'});
         }
         const senhaCriptografada = await bcrypt.hash(senha, 10);
         const usuario = await knex('usuarios').insert({
@@ -20,8 +23,37 @@ const cadastrarUsuario = async(req, res)=>{
         return res.status(201).json(novoUsuario);
     }catch(error){
         console.log(error)
-        return res.status(500).json({mensagem: 'Erro interno no servidor'});
+        return res.status(500).json({mensagem: 'Erro interno no servidor.'});
     }
 }
 
-module.exports = {cadastrarUsuario}
+const loginUsuario = async (req, res)=>{
+    const {email, senha} = req.body;
+
+    try{
+        const usuario = await knex('usuarios').where('email', email).first();
+
+        if(!usuario){
+            return res.status(400).json({mensagem: 'E-mail ou senha inválidos.'})
+        }
+
+        const senhaValida = await bcrypt.compare(senha, usuario.senha);
+        if(!senhaValida){
+            return res.status(400).json({mensagem: 'E-mail ou senha inválidos.'});
+        }
+        const token = jwt.sign({id: usuario.id},senhaJwt, {expiresIn: '1d'});
+
+        const {senha:_, usuarioLogado} = usuario;
+
+        return res.status(200).json({
+            usuario: usuarioLogado,
+            token
+        });
+
+    }catch(error){
+        return res.status(500).json({mensagem: 'Erro interno no servidor.'});
+    }
+
+}
+
+module.exports = {cadastrarUsuario, loginUsuario}
--- src/rotas.js
+++ src/rotas.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { cadastrarUsuario } = require('./controladores/usuarios');
+const { cadastrarUsuario, loginUsuario } = require('./controladores/usuarios');
 const validarRequisicao = require('./intermediarios/validacaoRequisicao');
 const usuarioSchema = require('./validacoes/usuarios');
+const loginSchema = require('./validacoes/login');
 const rotas = express();
 
 rotas.post('/usuario', validarRequisicao(usuarioSchema), cadastrarUsuario);
+rotas.post('/login', validarRequisicao(loginSchema),loginUsuario);
 
 
 module.exports = rotas;

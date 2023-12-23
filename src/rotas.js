30549dcfc3f22733aad7d3c6560aef8062268a00
Adriele Primo <adriele.dejprimo@gmail.com>
2023-12-23 10:03:32 -0300

    Criando autenticação de usuário

--- src/rotas.js
+++ src/rotas.js
@@ -3,10 +3,12 @@ const { cadastrarUsuario, loginUsuario } = require('./controladores/usuarios');
 const validarRequisicao = require('./intermediarios/validacaoRequisicao');
 const usuarioSchema = require('./validacoes/usuarios');
 const loginSchema = require('./validacoes/login');
+const verificarUsuarioLogado = require('./intermediarios/autenticacao');
 const rotas = express();
 
 rotas.post('/usuario', validarRequisicao(usuarioSchema), cadastrarUsuario);
 rotas.post('/login', validarRequisicao(loginSchema),loginUsuario);
 
+rotas.use(verificarUsuarioLogado);
 
 module.exports = rotas;
